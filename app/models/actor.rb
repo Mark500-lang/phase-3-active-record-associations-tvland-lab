de77f544511f1c19a5ead1ffb96594166ae1529a
Mark Kantet Sangiriaki <mark.sangiriaki@student.moringaschool.com>
2023-02-24 12:37:48 +0300

    completed

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,19 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    return "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    self.characters.each do |c|
+        self.shows.map do |s| 
+            if s.id == c.show_id
+                return ["#{c.name} - #{s.name}"]
+            end  
+        end
+    end
+    
+  end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
-  
-end
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+  def actors_list
+    self.actors.map do |e| 
+        return "#{e.first_name} #{e.last_name}"
+    end
+  end
+end#{self.actors.last_name}
